f23c7ef4a1c9c9f0085240abb59b4188c9c85f39
Pavelmd <pashanovosvit2@gmail.com>
2023-11-04 19:35:24 +0200

    save app css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import css from './StyleContainer.module.css';
+
+export const App = ({ children }) => {
+  return <div className={css.container}>{children}</div>;
 };

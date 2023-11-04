280b12cef6411f1dd3e77b4a0c0ca90963ac701f
Pavelmd <pashanovosvit2@gmail.com>
2023-11-04 20:51:00 +0200

    save buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,39 @@
 import css from './StyleContainer.module.css';
+import React, { Component } from 'react';
+import {Section} from './Section/Section'
+import { FeedbackOptions } from './FeedBackOptions/FeedbackOptions';
 
-export const App = ({ children }) => {
-  return <div className={css.container}>{children}</div>;
-};
+
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleFeedback = e => {
+    if (e === 'Good') {
+      this.setState({ good: this.state.good + 1 });
+    } else if (e === 'Neutral') {
+      this.setState({ neutral: this.state.neutral + 1 });
+    } else if (e === 'Bad') {
+      this.setState({ bad: this.state.bad + 1 });
+    }
+  };
+
+
+  render() {
+    return (
+      <div className ={css.container}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['Good', 'Neutral', 'Bad']}
+            onLeaveFeedback={this.handleFeedback}
+          />{' '}
+        </Section>
+      </div>
+      
+    )
+  }
+}
